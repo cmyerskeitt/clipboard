7c853197943d377041cf5defc8286e68b804bdfc
github username <github email address>
2021-04-21 21:34:16 -0400

    namespace routes

--- clipboard_backend/config/routes.rb
+++ clipboard_backend/config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :lessons
-  resources :days
+
+  namespace :api do
+    namespece :v1 do
+      resources :lessons
+      resources :days
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
