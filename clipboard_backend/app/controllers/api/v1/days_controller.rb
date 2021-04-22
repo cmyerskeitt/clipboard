d3c18269d7f6cb497a3380be2871cd758e401f06
github username <github email address>
2021-04-22 13:36:39 -0400

    setup day controller

--- clipboard_backend/app/controllers/api/v1/days_controller.rb
+++ clipboard_backend/app/controllers/api/v1/days_controller.rb
@@ -1,3 +1,28 @@
 class Api::v1::DaysController < ApplicationController
+
+    def index 
+        @days = Day.all
+        render json: @Days
+    end 
+
+    def create 
+        @day = Day.new(day_params) 
+        if @day.save
+            render json: @day
+        else
+            render json: {error: 'Error creating Day'}
+        end 
+    end 
+
+    def show 
+        @day = Day.find(params[:id])
+        render json: @day
+    end 
+
     
+    private
+
+    def day_params
+        params.require(:day).permit(:date)
+    end 
 end
--- clipboard_backend/app/models/day.rb
+++ clipboard_backend/app/models/day.rb
@@ -1,4 +1,4 @@
 class Day < ApplicationRecord
     has_many :lessons
-   
+    validates :name, presence: true 
 end
