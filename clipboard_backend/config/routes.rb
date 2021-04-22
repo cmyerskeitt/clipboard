b35a1b057f5923e6a95ff60ae28e80fbf220893d
github username <github email address>
2021-04-22 14:02:02 -0400

    setup lessons controller

--- clipboard_backend/config/routes.rb
+++ clipboard_backend/config/routes.rb
@@ -2,8 +2,10 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :lessons
-      resources :days
+      # resources :lessons
+      resources :days do
+        resources :lessons
+      end 
     end 
   end 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
