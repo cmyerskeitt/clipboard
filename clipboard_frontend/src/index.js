4dfcfd4e4a8709b6296de965b60977198cc9f729
github username <github email address>
2021-04-22 22:19:12 -0400

    setup index.js file and creted store

--- clipboard_frontend/src/index.js
+++ clipboard_frontend/src/index.js
@@ -5,8 +5,11 @@ import {createStore, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import {Provider} from 'react-redux'
 
+const store = createStore(reducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(applyMiddleware(thunk)))
+
 ReactDOM.render(
-    <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
-
