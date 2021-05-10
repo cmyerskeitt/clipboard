98d7d59de647503cf88d9d4bfa7bba0628640042
github username <github email address>
2021-05-10 15:41:22 -0400

    add button to change colors

--- clipboard_frontend/src/home/homeContainer.js
+++ clipboard_frontend/src/home/homeContainer.js
@@ -1,9 +1,25 @@
 import homeImage from './homeImage.jpg'
 import './home.css'
+import React from 'react'
 
-export default function HomeContainer(){
+class HomeContainer extends React.Component{
+    constructor(){
+        super()
+        this.state = {
+            black: true 
+        }
+    }
+
+    changeColor=()=>{
+        this.setState({
+           black: !this.state.black
+        })
+    }
+  
+    render(){
+        let changingClass = this.state.black ? 'background-black' : 'background-white'
     return (
-        <div >
+        <div className={changingClass} >
             <center>
             <h2>Welcome to Clipboard!</h2>
             <p>
@@ -13,8 +29,13 @@ export default function HomeContainer(){
             <img src={homeImage} alt="Clipboard on Wall" id="home-image"/><br></br>
             <small>
                 Photo source: <a href="https://images.unsplash.com/photo-1618514951866-efc95de03f37?ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzB8fGNsaXBib2FyZHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60">Luba Ertel</a>
-            </small>
+
+            </small> <br></br>
+            <button onClick={this.changeColor} > Change color </button>
             </center>
         </div>
     )
+  }
 }
+
+export default HomeContainer 
